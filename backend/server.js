8803db3932940bf5e827add8002314399f847eaa
tasnim-zeba <lorintasnimzeba@gmail.com>
2026-02-17 02:23:58 +0600

    [init] Initial boilerplate for MERN

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const app = require("./app");
+const dotenv = require("dotenv");
+const connectDatabase = require("./config/database")
+
+//Config
+dotenv.config({path:"backend/config/config.env"});
+
+connectDatabase();
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server is running on http://localhost:${process.env.PORT}`)
+}) 
