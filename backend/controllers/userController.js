21cb4ed00570568c9f290644d364d02f06e83110
tasnim-zeba <lorintasnimzeba@gmail.com>
2026-02-25 00:34:21 +0600

    [backend]logout,authentication,authorization,reset password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,7 @@ const ErrorHandler = require("../utils/errorhandler")
 const catchAsyncErrors = require("../middleware/catchAsyncErrors")
 const User = require("../models/userModel");
 const sendToken = require("../utils/jwtToken");
+const sendEmail = require("../utils/sendEmail");
 
 //Register a user
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
@@ -40,6 +41,95 @@ exports.loginUser = catchAsyncErrors(async (req, res, next) => {
     if (!isPasswordMatched) {
         return next(new ErrorHandler("Invalid email or password", 401));
     }
+
+    sendToken(user, 200, res);
+});
+
+//Logout user
+exports.logout = catchAsyncErrors(async (req, res, next) => {
+    res.cookie("token", null, {
+        expires: new Date(Date.now()),
+        httpOnly: true,
+    });
+
+    res.status(200).json({
+        success: true,
+        message: "Logged out",
+    });
+});
+
+//Forgot Password
+exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user) {
+        return next(new ErrorHandler("User not found with this email", 404));
+    }
+
+    //Get reset token
+    const resetToken = user.getResetPasswordToken();
+
+    await user.save({ validateBeforeSave: false });
+
+    //Create reset password url
+    const resetUrl = `${req.protocol}://${req.get(
+        "host"
+    )}/api/v1/password/reset/${resetToken}`;
+
+    const message = `Your password reset token is as follow:\n\n${resetUrl}\n\nIf you have not requested this email, then ignore it.`;
+
+    try {
+        await sendEmail({
+            email: user.email,
+            subject: "Ecommerce Password Recovery",
+            message,
+        });
+
+        res.status(200).json({
+            success: true,
+            message: `Email sent to ${user.email} successfully`,
+        });
+    } catch (error) {
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpire = undefined;
+
+        await user.save({ validateBeforeSave: false });
+
+        return next(new ErrorHandler(error.message, 500));
+    }
+});
+
+//Reset Password
+exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
+    //Hash URL token
+    const resetPasswordToken = crypto
+        .createHash("sha256")
+        .update(req.params.token)
+        .digest("hex");
+
+    const user = await User.findOne({
+        resetPasswordToken,
+        resetPasswordExpire: { $gt: Date.now() },
+    });
+
+    if (!user) {
+        return next(
+            new ErrorHandler(
+                "Reset Password Token is invalid or has been expired",
+                400
+            )
+        );
+    }
+
+    if (req.body.password !== req.body.confirmPassword) {
+        return next(new ErrorHandler("Password does not password", 400));
+    }
+
+    user.password = req.body.password;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save();
 
     sendToken(user, 200, res);
 });
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetails } = require("../controllers/productController");
+const { isAuthenticatedUser, athorizeRoles } = require("../middleware/auth");
 
 const router=express.Router();
 
 router.route("/products").get(getAllProducts)
-router.route("/product/new").post(createProduct)
-router.route("/product/:id").put(updateProduct).delete(deleteProduct).get(getProductDetails)
+router.route("/product/new").post(isAuthenticatedUser, athorizeRoles("admin"), createProduct)
+router.route("/product/:id").put(isAuthenticatedUser, athorizeRoles("admin"), updateProduct).delete(isAuthenticatedUser, athorizeRoles("admin"), deleteProduct).get(getProductDetails)
 
 
 
